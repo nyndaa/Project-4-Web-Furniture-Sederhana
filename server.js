e2de1fdbbba2d62c61c46a48d06f2c553fa61c89
nyndaa <raharjiansustanadiawijayanti@gmail.com>
2026-05-15 15:15:30 +0700

    Perubahan di bagian Server

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ function load(file) {
   return fs.readFileSync(path.join(__dirname, "views", file), "utf-8");
 }
 
-/*HOME*/
+/* HOME */
 app.get("/", (req, res) => {
+
   const navbar = load("navbar.html");
   const content = load("index.html");
   const footer = load("footer.html");
@@ -23,21 +24,23 @@ app.get("/", (req, res) => {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Furniture Shop</title>
 
-    <!-- CSS NYA DIKONEKIN DI SINI -->
     <link rel="stylesheet" href="/style.css">
 
     <!-- ICON -->
-  <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-  
-   <!--Font-->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- FONT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   </head>
+
   <body>
 
     ${navbar}
@@ -51,9 +54,6 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
-app.listen(PORT, () => {
-  console.log(`jalan di http://localhost:${PORT}`);
-});
 
 /* HALAMAN KATALOG */
 app.get("/katalog", (req, res) => {
@@ -102,6 +102,53 @@ app.get("/katalog", (req, res) => {
 });
 
 
+/* SERVER */
 app.listen(PORT, () => {
   console.log(`jalan di http://localhost:${PORT}`);
+});
+
+/* HALAMAN KONTAK */
+app.get("/kontak", (req, res) => {
+
+  const navbar = load("navbar.html");
+  const content = load("kontak.html");
+  const footer = load("footer.html");
+
+  const html = `
+  <!DOCTYPE html>
+  <html lang="id">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Kontak | QueenFurni</title>
+
+    <link rel="stylesheet" href="/style.css">
+
+    <!-- ICON -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- FONT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  </head>
+
+  <body>
+
+    ${navbar}
+    ${content}
+    ${footer}
+
+  </body>
+
+  </html>
+  `;
+
+  res.send(html);
+
 });
